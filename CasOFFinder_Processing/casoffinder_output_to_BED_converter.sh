897a8d25ec81f606ee55576da7e72f3bf96f93fa
NourHanafi <67124068+NourHanafi@users.noreply.github.com>
2023-02-14 14:43:11 -0500

    Update casoffinder_output_to_BED_converter.sh

--- CasOFFinder_Processing/casoffinder_output_to_BED_converter.sh
+++ CasOFFinder_Processing/casoffinder_output_to_BED_converter.sh
@@ -11,5 +11,5 @@ mkdir -p padded_bed_files
 
 for file in $(ls /hpf/largeprojects/lauryl/results/bcbio_nextgen/no_ADKT8417/sfilters/manuscript_revisions/casoffinder_output/casoffinder-new-reference/*casoffinder.output.txt); do 
 	ls $file; 
-	python step1acs_casoffinder_output_to_BED_converter.py $file
+	python acs_casoffinder_output_to_BED_converter.py $file
 done
