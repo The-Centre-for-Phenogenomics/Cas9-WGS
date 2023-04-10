9cda5e66ad2c610ed2ff449cfec72f0bb6a0f21e
NourHanafi <67124068+NourHanafi@users.noreply.github.com>
2023-04-10 09:50:29 -0400

    Update 2.Intersect_with_vcfs.sh
    Updated paths to reflect directory used for manuscript results.

--- CIRCLE-seq/2.Intersect_with_vcfs.sh
+++ CIRCLE-seq/2.Intersect_with_vcfs.sh
@@ -2,7 +2,7 @@
 
 module load bedtools
 
-mkdir -p circleseq_6mismatches_intersection_padded_Nov2022_ignoreStrands
+mkdir -p circleseq_6mismatches_intersection_padded_June2022_ignoreStrands
 
 BEDfilesFolder=/hpf/largeprojects/lauryl/results/bcbio_nextgen/no_ADKT8417/sfilters/circlesq/post-processing/bed_files
 
@@ -18,7 +18,7 @@ do
     for i in /hpf/largeprojects/lauryl/results/bcbio_nextgen/no_ADKT8417/samples/$a/applied_filters5_noZ/*_all78samples_ensemble_2padd.indels.2orMore.ADfilt.incJax2019.callableIntervals.applied_filters5_bcftools_isec/0000.vcf
     do 
         echo $i 
-        bedtools intersect -a $i -b $BEDfilesFolder/${b}_identified_matched.bed -wo > circleseq_6mismatches_intersection_padded_Nov2022_ignoreStrands/$a.$b.vcf_overlaps.txt 
+        bedtools intersect -a $i -b $BEDfilesFolder/${b}_identified_matched.bed -wo > circleseq_6mismatches_intersection_padded_June2022_ignoreStrands/$a.$b.vcf_overlaps.txt 
     done
 
 
@@ -26,14 +26,14 @@ do
     for i in /hpf/largeprojects/lauryl/results/bcbio_nextgen/no_ADKT8417/samples/$a/applied_filters5_noZ/*_all78samples_ensemble_2padd.snps.2orMore.ADfilt.incJax2019.callableIntervals.applied_filters5_bcftools_isec/0000.vcf
     do 
         echo $i
-        bedtools intersect -a $i -b $BEDfilesFolder/${b}_identified_matched.bed -wo >> circleseq_6mismatches_intersection_padded_Nov2022_ignoreStrands/$a.$b.vcf_overlaps.txt 
+        bedtools intersect -a $i -b $BEDfilesFolder/${b}_identified_matched.bed -wo >> circleseq_6mismatches_intersection_padded_June2022_ignoreStrands/$a.$b.vcf_overlaps.txt 
     done
 
 
     ## Metasv
     for i in /hpf/largeprojects/lauryl/results/bcbio_nextgen/new*/$a/final/*/*metasv_filtered.vcf.bedpe.noheader.fixed.sec_filt_ignoreStrands.1bp.start.bed12.notrack.0bp.start.bed12
     do echo $i
-        bedtools intersect -a $i -b $BEDfilesFolder/${b}_identified_matched.bed -wo >> circleseq_6mismatches_intersection_padded_Nov2022_ignoreStrands/$a.$b.vcf_overlaps.txt
+        bedtools intersect -a $i -b $BEDfilesFolder/${b}_identified_matched.bed -wo >> circleseq_6mismatches_intersection_padded_June2022_ignoreStrands/$a.$b.vcf_overlaps.txt
     done
 
     echo
